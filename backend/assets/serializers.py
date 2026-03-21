2519042f000825a98c5f7192ae64c710435e315e
inta <intasolutionpvtltd@gmail.com>
2026-03-21 14:13:25 +0530

    Fix serializer order to avoid runtime 500 error

--- backend/assets/serializers.py
+++ backend/assets/serializers.py
@@ -2,16 +2,16 @@ from rest_framework import serializers
 from .models import AssetCategory, Asset, AssetMaintenance, AssetAssignmentHistory
 from django.contrib.auth.models import User
 
-class UserSimpleSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = User
-        fields = ('id', 'username', 'first_name', 'last_name', 'email')
-
 class AssetCategorySerializer(serializers.ModelSerializer):
-    assets_count = serializers.IntegerField(source='assets.count', read_only=True)
-    
     class Meta:
         model = AssetCategory
+        fields = '__all__'
+
+class AssetAssignmentHistorySerializer(serializers.ModelSerializer):
+    username = serializers.CharField(source='assigned_to.username', read_only=True)
+    
+    class Meta:
+        model = AssetAssignmentHistory
         fields = '__all__'
 
 class AssetMaintenanceSerializer(serializers.ModelSerializer):
@@ -19,19 +19,21 @@ class AssetMaintenanceSerializer(serializers.ModelSerializer):
         model = AssetMaintenance
         fields = '__all__'
 
-class AssetAssignmentHistorySerializer(serializers.ModelSerializer):
-    user_detail = UserSimpleSerializer(source='user', read_only=True)
-    
-    class Meta:
-        model = AssetAssignmentHistory
-        fields = '__all__'
-
 class AssetSerializer(serializers.ModelSerializer):
-    category_name = serializers.ReadOnlyField(source='category.name')
-    assigned_to_detail = UserSimpleSerializer(source='assigned_to', read_only=True)
-    maintenance_history = AssetMaintenanceSerializer(source='maintenances', many=True, read_only=True)
-    assignment_history = AssetAssignmentHistorySerializer(source='assignment_history', many=True, read_only=True)
-    
+    category_name = serializers.CharField(source='category.name', read_only=True)
+    assigned_to_detail = serializers.SerializerMethodField()
+    maintenance_records = AssetMaintenanceSerializer(many=True, read_only=True, source='maintenance_history')
+    assignment_history = AssetAssignmentHistorySerializer(many=True, read_only=True)
+
     class Meta:
         model = Asset
         fields = '__all__'
+
+    def get_assigned_to_detail(self, obj):
+        if obj.assigned_to:
+            return {
+                'id': obj.assigned_to.id,
+                'username': obj.assigned_to.username,
+                'email': obj.assigned_to.email
+            }
+        return None
